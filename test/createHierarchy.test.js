b88efdcf542b236aeccf6b1ad07b64b234a69ecb
Fan, Yi-Jie <4645715+fancn21th@users.noreply.github.com>
2022-02-28 20:19:05 +0800

    test(createhierarchy): 增加一个3个节点的测试用例 (#27)

--- test/createHierarchy.test.js
+++ test/createHierarchy.test.js
@@ -27,4 +27,23 @@ describe("createHierarchy test", function () {
 
     expect(result).to.deep.equal(expected);
   });
+
+  it("has three tree nodes | 3个节点 且 乱序", function () {
+    const source = [
+      { id: 2, title: "武汉", pid: 1 },
+      { id: 0, title: "中国", pid: null },
+      { id: 1, title: "湖北", pid: 0 },
+    ];
+
+    //  https://www.chaijs.com/api/bdd/
+
+    //  深度比较
+    // expect({a: 1}).to.deep.equal({a: 1});
+
+    const result = createHierarchy(source);
+
+    // 中国 children 属性中 有 湖北
+    // TODO: 此处有问题
+    expect(result.find((item) => item.id === 0)).to.have.property("children");
+  });
 });
